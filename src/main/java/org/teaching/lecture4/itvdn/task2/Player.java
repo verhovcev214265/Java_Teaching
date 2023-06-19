384fc8301082164ca7442851d18b8032ed42a4a3
Mark <verhovcev214265@gmail.com>
2023-06-19 10:12:22 +0200

    Lecture 4 before Vlad's feedback.

--- src/main/java/org/teaching/lecture4/itvdn/task2/Player.java
+++ src/main/java/org/teaching/lecture4/itvdn/task2/Player.java
@@ -1,5 +1,12 @@
 package org.teaching.lecture4.itvdn.task2;
 
+/*
+Создайте 2 интерфейса Playable и Recording. В каждом из интерфейсов создайте по 3 метода void play() /
+void pause() / String stop() и String record() / String pause() / String stop() соответственно.
+Создайте производный класс Player от базовых интерфейсов Playable и Recording.
+Написать программу, которая выполняет проигрывание и запись.
+ */
+
 public class Player implements  Recording{
 
     @Override
