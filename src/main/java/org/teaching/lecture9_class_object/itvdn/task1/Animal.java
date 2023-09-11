2428b4c43202d0dd0a1785a625baae905aca018d
Mark <verhovcev214265@gmail.com>
2023-09-11 08:35:12 +0200

    itvdn first task has completed

---
+++ src/main/java/org/teaching/lecture9_class_object/itvdn/task1/Animal.java
@@ -0,0 +1,36 @@
+package org.teaching.lecture9_class_object.itvdn.task1;
+
+import java.util.Objects;
+
+/*
+Создать класс Animal с именем String, возрастом int, хвостом Boolean. В классе переопределить метод
+toString, что бы вывод был сдедующим
+«Имя: Bob, возраст: 12, хвост: true».
+В классе Animal переопределить методы equals & hashCode.
+ */
+
+public class Animal {
+
+    private final String name;
+    private final int age;
+    private final boolean tail;
+
+    public Animal(String name, int age, boolean tail) {
+        this.name = name;
+        this.age = age;
+        this.tail = tail;
+    }
+
+    @Override
+    public boolean equals(Object other){
+        if (this == other) return true;
+        if (!(other instanceof Animal)) return false;
+        Animal animal = (Animal) other;
+        return age == animal.age && tail == animal.tail && Objects.equals(name, animal.name);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(name, age, tail);
+    }
+}
---
+++ src/main/java/org/teaching/lecture9_class_object/itvdn/task2/Device.java
@@ -0,0 +1,10 @@
+package org.teaching.lecture9_class_object.itvdn.task2;
+
+/*
+1) Основной класс Device (manufacturer(String), price(float), serialNumber(String));
+2) Сабкалсс Monitor (resolutionX(int), resolutionY(int)) and EthernetAdapter (speed (int), mac (String));
+Добавить методы доступа. Конструктор.
+ */
+
+public class Device {
+}
